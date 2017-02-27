a74be22c615a5a03befaeb4dba0e4c6f52203821
Bryan P. Cole <cole.bryan.p@gmail.com>
2017-02-26 19:05:14 -0800

    Uploader Files

---
+++ app/controllers/assets_controller.rb
@@ -0,0 +1,74 @@
+class AssetsController < ApplicationController
+  before_action :authenticate_user!,:set_asset, only: [:show, :edit, :update, :destroy]
+
+  # GET /assets
+  def index
+    @assets = current_user.assets
+  end
+
+  # GET /assets/1
+  def show
+    @asset = current_user.assets.find(params[:id])
+  end
+
+  # GET /assets/new
+  def new
+    @asset = current_user.assets.new
+  end
+
+  # GET /assets/1/edit
+  def edit
+    @asset = current_user.assets.find(params[:id])
+  end
+
+  # POST /assets
+  def create
+    @asset = current_user.assets.new(asset_params)
+
+    respond_to do |format|
+      if @asset.save
+        format.html { redirect_to @asset, notice: 'File was successfully uploaded.' }
+        format.json { render :show, status: :created, location: @asset }
+      else
+        format.html { render :new }
+        format.json { render json: @asser.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /assets/1
+  def update
+    @asset = current_user.assets.find(params[:id])
+
+    respond_to do |format|
+      if @asset.update(pet_params)
+        format.html { redirect_to @asset, notice: 'File was successfully updated.' }
+        format.json { render :show, status: :ok, location: @asset }
+      else
+        format.html { render :edit }
+        format.json { render json: @asset.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /assets/1
+  def destroy
+    @asset = current_user.assets.find(params[:id])
+    @asset.destroy
+    respond_to do |format|
+      format.html { redirect_to assets_url, notice: 'File was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_asset
+      @asset = Asset.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def asset_params
+      params.require(:asset).permit(:file_upload)
+    end
+end
