08d5c9eeb84c3c87a75e4d98dab6a08bb3dbc15a
Bryan P. Cole <cole.bryan.p@gmail.com>
2017-03-04 21:42:10 -0800

    Minor layout update and signed in route change
    Added the current possible logo and set the root route to change based on whether a user was signed in. If a user is signed in it goes to the assets index, otherwise it routes to the original home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :assets
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html    
-  root :to => "home#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root 'assets#index', as: :authenticated_root
+  end
+    root :to => "home#index"
   devise_scope :user do  
    get '/users/sign_out' => 'devise/sessions#destroy'     
 end
