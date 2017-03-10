7970e258ba2d5ed0f81574de82e19aee46c6b487
Andrew Hu <primoriscruor1@gmail.com>
2017-03-10 12:30:42 -0800

    ENV varables

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -6,8 +6,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'], 
+      :aws_secret_access_key  => ENV['S3_SECRET'],
       :region                 => 'us-west-2' # Change this for different AWS region. Default is 'us-east-1'
   }
+  config.fog_directory  = 'elasticbeanstalk-us-west-2-961324297475'
 end
 # To enable root element in JSON for ActiveRecord objects.
 # ActiveSupport.on_load(:active_record) do
