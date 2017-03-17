045592776cd140f794ec204b4d91c9110e7f1b5e
Anthony Tran <anthonytran94538@gmail.com>
2017-03-17 14:14:36 -0700

    step 6

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -6,7 +6,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY'], 
+      :aws_access_key_id      => ENV['S3_KEY'],
       :aws_secret_access_key  => ENV['S3_SECRET'],
       :region                 => 'us-west-2' # Change this for different AWS region. Default is 'us-east-1'
   }
