f882d0f44cd8ac8793e501ca84ababf6c40ed9df
Anthony Tran <anthonytran94538@gmail.com>
2017-03-17 14:08:33 -0700

    step 6

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
-class HomeController < ApplicationController 
-    
-  def index 
-     
+class HomeController < ApplicationController
+  def index
+    if user_signed_in?
+      @assets = current_user.assets.order("uploaded_file_file_name desc")
+    end
   end
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,7 @@
 class Asset < ApplicationRecord
   belongs_to :user
   mount_uploader :file_upload, FileUploader
+  def file_size
+    uploaded_file_file_size
+  end
 end
